f04a13d2dac6cd7096a0e5ef2f7fd28f17c36845
Lalithvignesh24 <andhelalithvignesh@gmail.com>
2026-06-07 15:33:15 +0530

    Front-end url change

--- client/src/services/socketService.js
+++ client/src/services/socketService.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:3001";
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "https://guessink.onrender.com";
 
 let socketInstance;
 
